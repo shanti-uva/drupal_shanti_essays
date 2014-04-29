86edd18d32ad3f11db722781b22c53aa5c6b08f9
Rafael Alvarado <rca2t@virginia.edu>
2014-04-29 13:03:57 -0400

    More stuff.

--- js/shanti_essays.js
+++ js/shanti_essays.js
@@ -3,14 +3,51 @@
 Drupal.behaviors.shantiEssays = {
   attach: function (context, settings) {
 
-    // Just testing things out ...
-    $('#shanti-essay-page-marginalia').html("We're in. Now what?");
-    
     // Get the actual height of the containing div for the SHANTI Essays 'app'
     // and set the TOC div to that height in CSS
-    divHeight = $('#node-' + Drupal.settings.shantiEssays.book.nid).height();
-    $('.node-book').css('height', divHeight);
-  
+    //divHeight = $('#node-' + Drupal.settings.shantiEssays.book.nid).height();
+    //$('.node-book').css('height', divHeight + 50);
+    
+    // Keep the nav bar at the top
+    var bookHeader = $("#shanti-essay-page-content-body-header");
+    var o = bookHeader.offset();  
+    var w = bookHeader.width();    
+    var h = bookHeader.height();
+
+    /*
+    var bookToc = $("#shanti-essay-page-toc");
+    var o1 = bookToc.offset();  
+    var w1 = bookToc.width();    
+    var h1 = bookToc.height();
+    */
+
+    var x = 0; 
+    if ($('#admin-menu').length) x = $('#admin-menu').height();
+    //$('#shanti-essay-page-content-body').css("margin-top",h+10);   
+
+    $(window).scroll(function() {
+      var wpos = $(window).scrollTop();
+      if (wpos >= o.top) {
+          
+          bookHeader.css("position","fixed");
+          bookHeader.css("top",x);
+          bookHeader.css("width",w);
+          /*
+          bookToc.css("position","fixed");
+          bookToc.css("top",x);
+          bookToc.css("width",w1);
+          */
+          
+      } else {
+          bookHeader.css("position","absolute");
+          /*
+          bookToc.css("position","absolute");
+          bookToc.css("margin-left","6.5in");
+          */
+      }
+      
+    });
+    
   }
 };
   
--- templates/node--book.tpl.php
+++ templates/node--book.tpl.php
@@ -1,47 +1,20 @@
-<?php //kpr($content); ?>
 <?php //kpr($node); ?>
 <?php //kpr($book); ?>
 <?php //kpr($shanti_essays); ?>
+<?php //kpr($content['links']['book']); ?>
 
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-<!-- MODEL:
-node
-  shanti-essay-page-toc
-  shanti-essay-page-content
-    shanti-essay-page-content-body
-    shanti-essay-page-content-body-footer
-      shanti-essay-page-links
-      shanti-essay-page-navigation 
---> 
-
 <div id="shanti-essay-page-content">
+  <div id="shanti-essay-page-content-body-header">
+    <div id="shanti-essay-page-navigation"><?php print render($content['book_navigation']); ?></div>    
+    <!-- <div id="shanti-essay-page-links"><?php print render($content['links']); ?></div> -->
+  </div>
   <div id="shanti-essay-page-content-body">
-  <?php print render($content['links']); ?>
-  <?php print render($content['body']); ?>
+    <?php print render($content['field_book_content'][0]['#markup']); ?>
   </div>
   <div id="shanti-essay-page-content-body-footer">
-    <div id="shanti-essay-page-navigation"><?php print render($content['book_navigation']); ?></div>    
-    <div id="shanti-essay-page-links"><?php print render($content['links']); ?></div>
-  </div>
-</div>
-
-<div id="shanti-essay-page-toc">
-<?php print "<a href='". $shanti_essays['book_url'] ."'>" . $shanti_essays['book_title'] . "</a>"; // Should be put in a render array, I know. ?>
-<?php print render(book_explorer_block_view()); ?>
-</div>
-
-
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>>
-      <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
-    </h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-
-  <div id="shanti-essay-page-extra">
+    <div id="shanti-essay-page-extra">
   
   <?php
     // Remove the "Add new comment" link on the teaser page or if the comment
@@ -53,8 +26,9 @@ node
     $links = render($content['links']);
     if ($links):
   ?>
-  <div class="link-wrapper">
-      <?php print $links; ?>
+  
+    <div class="link-wrapper">
+      <?php // print $links; ?>
     </div>
   <?php endif; ?>
 
@@ -65,25 +39,23 @@ node
     </div>
   <?php endif; ?>  
   </div>
-  
-<!--
-  <div class="content clearfix"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
+
   </div>
-  <?php
-    // Remove the "Add new comment" link on the teaser page or if the comment
-    // form is being displayed on the same page.
-    if ($teaser || !empty($content['comments']['comment_form'])) {
-      unset($content['links']['comment']['#links']['comment-add']);
-    }
-  ?>
-  <?php print render($content['comments']); ?>
--->
+</div>
+
+<div id="shanti-essay-page-toc">
+  <?php print "<a class='book-outline-title' href='". $shanti_essays['book_url'] ."'>" . $shanti_essays['book_title'] . "</a>"; // Should be put in a render array, I know. ?>
+  <?php print render(book_explorer_block_view()); ?>
+  <?php print render($content['links']['book']); ?>
+</div>
 
 
+<div>
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>>
+      <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+    </h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
 </div>
