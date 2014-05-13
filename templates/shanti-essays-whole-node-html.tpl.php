601c84ff628326f66aa792ab85103f50aa8aead2
Rafael Alvarado <rca2t@virginia.edu>
2014-05-13 16:56:33 -0400

    Moved whole page TOC generation to server side.

--- templates/shanti-essays-whole-node-html.tpl.php
+++ templates/shanti-essays-whole-node-html.tpl.php
@@ -18,8 +18,19 @@
  * @ingroup themeable
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="section-<?php print $depth; ?>">
+<div id="node-<?php print $node->nid; ?>" class="book-section section-<?php print $depth; ?>">
+  
   <h1 class="book-heading"><?php print $title; ?></h1>
+  
+  <div class="book-edit-menu">
+    <a class='head-toggle action'>&#10038;</a>
+    <a class='toc-return action' href='#toc'>&#10138;</a>
+  </div>
+  
+  <div class="book-content">
   <?php print $content; ?>
+  </div>
+  
   <?php print $children; ?>
+  
 </div>
