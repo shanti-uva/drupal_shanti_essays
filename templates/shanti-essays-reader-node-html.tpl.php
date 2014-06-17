b089b3075da1454b07938a4c4a72b9d8ac801d42
root <root@drupal-dev-s.shanti.virginia.edu>
2014-06-17 11:38:18 -0400

    Removed experimental code and files.

--- templates/shanti-essays-reader-node-html.tpl.php
+++
@@ -1,6 +0,0 @@
-<div id="node-<?php print $node->nid; ?>" class="book-node level-<?php print $depth; ?>">
-  <h1 class="book-heading"><?php print $title; ?></h1>
-  <div class="book-edit-menu"></div>
-  <?php print $content; ?>
-  <?php print $children; ?>
-</div>
