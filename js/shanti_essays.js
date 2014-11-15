c3dfe6c5b9a9d3f51c36d7fe51e43c68534a7f3b
Rafael Alvarado <rca2t@virginia.edu>
2014-11-14 19:28:02 -0500

    Migrated sidebar tabs to Bootstrap.

--- js/shanti_essays.js
+++ js/shanti_essays.js
@@ -3,9 +3,11 @@
 Drupal.behaviors.shantiEssays = {
   
   attach: function (context, settings) {   
-    $('#shanti-essays-sidebar').tabs();
-    $('#shanti-essays-sidebar').css('display','block');
-        
+    
+    $('#shanti-essays-sidebar-tabs li.first a').tab('show'); // Bootstrap
+    
+    $('#shanti-essays-sidebar').css('display','block');      
+
     $('.toc-item-node', context).localScroll({
       target:'#shanti-essays-body',
       duration: 10,
