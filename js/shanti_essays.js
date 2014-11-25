e1825c1c167d8001c4ed2e1eeaf1df8c60f52551
Rafael Alvarado <rca2t@virginia.edu>
2014-11-25 17:17:45 -0500

    Mega Search sorts properly now.

--- js/shanti_essays.js
+++ js/shanti_essays.js
@@ -3,6 +3,15 @@
 Drupal.behaviors.shantiEssays = {
   
   attach: function (context, settings) {   
+  
+    var kwic_n = Drupal.settings.shantiEssays.kwic_n;
+    if (kwic_n) {
+      // If there is a kwic number ...
+      location.href = '#' + 'shanti-essays-search-hit-' + kwic_n;
+    } else {
+      // This handles scrolling to anchors which doesn't otherwise work  
+      location.href = '#' + window.location.hash.substr(1);
+    }
     
     $('#shanti-essays-sidebar-tabs li.first a').tab('show'); // Bootstrap
     
