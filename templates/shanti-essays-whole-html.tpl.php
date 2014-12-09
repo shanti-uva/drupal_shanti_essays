a0fa2591edf1cdf5a4e1c8c270afa99ea9663643
Rafael Alvarado <rca2t@virginia.edu>
2014-12-09 17:43:56 -0500

    Significant clean up.

--- templates/shanti-essays-whole-html.tpl.php
+++ templates/shanti-essays-whole-html.tpl.php
@@ -4,9 +4,6 @@
     <span class="toc-action openonly" id="toc-adjust-width">
     	<i class="fa fa-caret-square-o-left fa-lg"></i>
     </span>
-  <!--
-    <span class="toc-action" id="toc-collapse-toggle"><i class="fa fa-chevron-left fa-lg"></i></span>
-  -->
     <span class="toc-action" id="toc-collapse-toggle-h"><i class="fa fa-bars fa-lg"></i></span>
     <span class="toc-action" id="toc-reorder"><?php print drupal_render($toc_menu['links']['toc-reorder']); ?></span>
     <span class="toc-action" id="toc-go-home"><?php print drupal_render($toc_menu['links']['toc-go-home']); ?></span>
