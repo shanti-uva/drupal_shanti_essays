84450754110db91c04d38628fec45776c3093c8e
Rafael Alvarado <rca2t@virginia.edu>
2014-12-18 12:54:49 -0500

    Removed SHANTI Viewer :-(

--- templates/shanti-essays-whole-node-html.tpl.php
+++
@@ -1,6 +0,0 @@
-<div id="book-node-<?php print $node->nid; ?>" class="book-section section-<?php print $depth; ?>">
-  <h1 class="book-heading"><?php print $title; ?></h1>  
-  <?php #print render($book_edit_menu); ?>
-  <?php print $content; ?>
-  <?php print $children; ?>
-</div>
